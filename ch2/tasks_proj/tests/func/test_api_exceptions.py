e3390c4e05eb0e796867d4f6b5e4743d95602571
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-12 04:06:17 +0900

    flip it back

--- ch2/tasks_proj/tests/func/test_api_exceptions.py
+++ ch2/tasks_proj/tests/func/test_api_exceptions.py
@@ -49,6 +49,6 @@ def test_delete_raises():
 def test_start_tasks_db_raises():
     """Make sure unsupported db raises an exception."""
     with pytest.raises(ValueError) as excinfo:
-        tasks.start_tasks_db('some/great/path', 'mongo')
+        tasks.start_tasks_db('some/great/path', 'mysql')
     exception_msg = excinfo.value.args[0]
-    assert exception_msg == "db_tdype must be a 'tiny' or 'mongo'"
+    assert exception_msg == "db_type must be a 'tiny' or 'mongo'"
