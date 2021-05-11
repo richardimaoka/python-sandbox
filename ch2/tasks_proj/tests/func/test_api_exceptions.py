c96a734953a369d6b220170a88e16a32a6e93cc2
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-12 04:11:22 +0900

    experimenting valid add

--- ch2/tasks_proj/tests/func/test_api_exceptions.py
+++ ch2/tasks_proj/tests/func/test_api_exceptions.py
@@ -7,18 +7,18 @@ import tasks
 def test_add_raises():
     """add() should raise an exception with wrong type param."""
     with pytest.raises(TypeError):
-        tasks.add(task='not a Task object')
+        tasks.add(task=tasks.Task('do something', 'okken', True, None))
 
 
-@pytest.mark.smoke
+@ pytest.mark.smoke
 def test_list_raises():
     """list() should raise an exception with wrong type param."""
     with pytest.raises(TypeError):
         tasks.list_tasks(owner=123)
 
 
-@pytest.mark.get
-@pytest.mark.smoke
+@ pytest.mark.get
+@ pytest.mark.smoke
 def test_get_raises():
     """get() should raise an exception with wrong type param."""
     with pytest.raises(TypeError):
