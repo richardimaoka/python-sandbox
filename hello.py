0d01f2f21baa982ec78853d7e5332e16d099a208
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-13 17:34:52 +0900

    perfect python 2nd chap

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-def hello_world(night=False):
-    if night:
-        print('Good evening world!')
-    else:
-        print('Hello World!')
+def main():
+    """サンプルプログラムのメイン関数"""
+    for i in range(1, 6):
+        if i % 2 == 0:
+            print("%s is an even number" % i)
+        else:
+            print("%s is an odd number" % i)
+
+
+if __name__ == "__main__":
+    main()
