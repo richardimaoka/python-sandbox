9ea8881d1f7326301566bfb660317f0dd68e2430
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-06 13:27:50 +0900

    more on formatting

--- hello.py
+++ hello.py
@@ -9,11 +9,17 @@ def main():
     data = {'x': 4, 'y': 5}
     print("{0[x]}, {0[y]}".format(data))
 
+    print(format(p))
+    print(format(p, "04"))
+
 
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __format__(self, spec):
+        return ("({0.x:" + spec + "}, {0.y:" + spec + "})").format(self)
 
 
 if __name__ == "__main__":
