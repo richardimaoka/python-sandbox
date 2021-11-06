0ac122859d9d6555ba75c465c42ac16eccf440cc
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-06 11:39:35 +0900

    dictionary access

--- hello.py
+++ hello.py
@@ -6,6 +6,9 @@ def main():
     print("{0.x}, {0.y}".format(p))
     print("{0}, {1}".format(p.x, p.y))
 
+    data = {'x': 4, 'y': 5}
+    print("{0[x]}, {0[y]}".format(data))
+
 
 class Point:
     def __init__(self, x, y):
