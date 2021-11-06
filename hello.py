95ff21bbae03a1dfd1e97ae7144ac177b45dd98a
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-06 15:09:56 +0900

    avoid extra newline by print

--- hello.py
+++ hello.py
@@ -2,21 +2,12 @@ import sys
 import fileinput
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-e', '--encoding', default=None)
-parser.add_argument('search', help="search word")
-parser.add_argument('files', nargs="+")
-args = parser.parse_args()
-search = args.search
+
+def main():
+    with open('products.txt') as f:
+        for line in f:
+            sys.stdout.write(line)
 
 
-def enc_open(filename, mode):
-    return open(filename, mode=mode, encoding=args.encoding)
-
-
-with fileinput.FileInput(files=args.files,
-                         openhook=enc_open) as f:
-    for line in f:
-        if line.find(search) > -1:
-            print("{0:20}:{1:4} {2}".format(
-                  f.filename(), f.lineno(), line))
+if __name__ == '__main__':
+    main()
