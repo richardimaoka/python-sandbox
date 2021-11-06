d57d0b36d5ec84f412e4a83e1aca84ae8756b3b3
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-06 15:26:23 +0900

    in progress

--- hello.py
+++ hello.py
@@ -6,7 +6,13 @@ import argparse
 def main():
     with open('products.txt') as f:
         for line in f:
-            sys.stdout.write(line)
+            parts = line.split(',')
+            title = parts[0]
+            count = int(parts[1])
+            results = {}
+            last_count = results.get(title, 0)
+            results[title] = last_count + count
+            print(results)
 
 
 if __name__ == '__main__':
