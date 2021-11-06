9c2313a365c910af486b09d58e0dbe4ea99fa4f0
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-06 11:34:24 +0900

    comparing property access in format

--- hello.py
+++ hello.py
@@ -2,31 +2,15 @@ import sys
 
 
 def main():
-    print("{0:04} {1:02}".format(2, 8))
-    print("{0:05} {1:02}".format(2, 8))
-    print("{0:06} {1:02}".format(2, 8))
-    print("{0:6} {1:02}".format(232, 8))
-    print("{0:6} {1:02}".format(232.3, 8))
-    print("{0:06} {1:02}".format(232.3, 8))
-    # invalid
-    # print("{0:0x6} {1:02}".format(232.3, 8))
-    # invalid
-    # print("{0:x6} {1:02}".format(232.3, 8))
-    print("{0:04} {1:02}".format(2, 8))
-    print("{0:04} {0:02}".format(2, 8))
-    # invlid
-    # print("{0:04} {3:02}".format(2, 8))
-    print("{0:,}".format(9999999999999999))
+    p = Point(4, 5)
+    print("{0.x}, {0.y}".format(p))
+    print("{0}, {1}".format(p.x, p.y))
 
 
-def echo(in_, out):
-    for line in in_:
-        out.write("{0} {1}".format(len(line), line))
-
-
-def docstring_test():
-    """this is for documentation"""
-    return True
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 if __name__ == "__main__":
