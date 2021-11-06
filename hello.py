b215baa0797f8b1134e7e5c985517b8cb28b5b48
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-06 11:25:57 +0900

    various formatting

--- hello.py
+++ hello.py
@@ -3,6 +3,20 @@ import sys
 
 def main():
     print("{0:04} {1:02}".format(2, 8))
+    print("{0:05} {1:02}".format(2, 8))
+    print("{0:06} {1:02}".format(2, 8))
+    print("{0:6} {1:02}".format(232, 8))
+    print("{0:6} {1:02}".format(232.3, 8))
+    print("{0:06} {1:02}".format(232.3, 8))
+    # invalid
+    # print("{0:0x6} {1:02}".format(232.3, 8))
+    # invalid
+    # print("{0:x6} {1:02}".format(232.3, 8))
+    print("{0:04} {1:02}".format(2, 8))
+    print("{0:04} {0:02}".format(2, 8))
+    # invlid
+    # print("{0:04} {3:02}".format(2, 8))
+    print("{0:,}".format(9999999999999999))
 
 
 def echo(in_, out):
