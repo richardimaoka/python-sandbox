6a0bde29f5a1aa19eb7c190e7ff89f18769e0e28
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-09 23:47:36 +0900

    sorted

--- hello.py
+++ hello.py
@@ -1,18 +1,28 @@
 import sys
 import fileinput
 import argparse
+import operator
+
+
+def sum_products(filename):
+    with open(filename) as f:
+        results = {}
+        for line in f:
+            parts = line.split(",")
+            title = parts[0]
+            count = int(parts[1])
+            last_count = results.get(title, 0)
+            results[title] = last_count + count
+    return results
+
+
+def print_results(results):
+    for key, value in sorted(results.items(), key=operator.itemgetter(1)):
+        print(key, value)
 
 
 def main():
-    with open('products.txt') as f:
-        for line in f:
-            parts = line.split(',')
-            title = parts[0]
-            count = int(parts[1])
-            results = {}
-            last_count = results.get(title, 0)
-            results[title] = last_count + count
-            print(results)
+    print_results(sum_products('products.txt'))
 
 
 if __name__ == '__main__':
