aa43736506cfe7389c7494d8f8b89b07c837ce0a
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-18 23:11:36 +0900

    route guide start coding

---
+++ route_guide_server.py
@@ -0,0 +1,42 @@
+from concurrent import futures
+import logging
+import grpc
+import route_guide_pb2
+import route_guide_pb2_grpc
+import route_guide_resources
+
+
+def get_feature(feature_db, point):
+    """Returns Feature at given location or None"""
+    for feature in feature_db:
+        if feature.location == point:
+            return feature
+    return None
+
+
+class RouteGuideServicer(route_guide_pb2_grpc.RouteGuideServicer):
+    """Provides methods that implement functionality of route guide server"""
+
+    def __init__(self):
+        self.db = route_guide_resources.read_route_guide_database()
+
+    def GetFeature(self, request, context):
+        feature = get_feature(self.db, request)
+        if feature is None:
+            return route_guide_pb2.Feature(name="", location=request)
+        else:
+            return feature
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    route_guide_pb2_grpc.add_RouteGuideServicer_to_server(
+        RouteGuideServicer, server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
