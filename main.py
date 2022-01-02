1670ad3a645d20674b2593cfce07a2333af69232
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-01-03 02:26:54 +0900

    some stuff

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-text: str = 1
+import typing
+from collections import namedtuple
+
+
+class Item(typing.NamedTuple):
+    user: str
+    fish: str
+    amount: int
+    wasabi: bool
+
+
+Item = namedtuple("Item", ["user", "fish", "amount", "wasabi"])
+
+
+def add(a, b):
+    return a + b
+
+
+def f():
+    return add(None, 0)
+
+
+def add(a, b):
+    c = a if a else 0
+    return a + b
