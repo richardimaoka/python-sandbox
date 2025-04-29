ea48111adb8cca1d1ad4109706f1fbbebbf3e369
richardimaoka <richard.s.imaoka@gmail.com>
2025-04-29 18:31:23 +0900

    asyncio hello world

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from hello-world!")
+import asyncio
+import time
 
+async def hello_world(n):
+    time.sleep(1)
+    print("{}: Hello World!".format(n))
 
-if __name__ == "__main__":
-    main()
+async def call_hello_world1():
+    print("call_hello_world1()")
+    await hello_world(1)
+
+async def call_hello_world2():
+    print("call_hello_world2()")
+    await hello_world(2)
+
+loop = asyncio.get_event_loop()
+loop.create_task(call_hello_world1())
+loop.run_until_complete(call_hello_world2())
